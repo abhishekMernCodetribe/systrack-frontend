471c75bb5ab7603bf76d3a1b4ad932ae8f2d5268
Codetribe Solutions Pvt. Ltd <>
2025-07-28 18:11:58 +0530

    updated route

--- src/components/SuperAdmin.jsx
+++ src/components/SuperAdmin.jsx
@@ -72,13 +72,7 @@ const SuperAdmin = () => {
     }
 
     if (!authorized) {
-        return (
-            <div className="min-h-screen flex items-center justify-center bg-gray-100">
-                <h1 className="text-2xl font-semibold text-red-500">
-                    Unauthorized access
-                </h1>
-            </div>
-        );
+        navigate("/");
     }
 
     const getAllEmployee = async () => {
